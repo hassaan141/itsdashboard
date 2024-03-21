07c0f7413e8038ce89a1a2b4659946b53c9102b8
Mustafa Rizvi <mustafa_rizvi@hotmail.com>
2024-03-21 15:18:49 -0400

    EVENTS MONITORING LIST updating

--- src/Events/EventsMonitoring.jsx
+++ src/Events/EventsMonitoring.jsx
@@ -59,8 +59,6 @@ import React, { useEffect, useState } from 'react';
 import './EventsMonitoring.css';
 import { useData } from '../sendBackEnd/dataContext';
 
-let camera, congType, start, end;
-
 const EventsMonitoring = () => {
   const [events, setEvents] = useState([]);
   const { responseData } = useData(); // Assuming this provides the data correctly
@@ -68,23 +66,31 @@ const EventsMonitoring = () => {
   
   useEffect(() => {
     console.log("Response Data:", responseData); 
+  
     if (responseData && responseData.success) {
-      console.log(responseData)
-      // const eventsArray = Object.values(responseData.data);
-      // console.log(eventsArray);
-      const loadedEvents = responseData.data.map((event) => ({ // Removed .data to correctly map over eventsArray
-        //id: index, // Consider using a unique identifier from your data if available
-        camera: responseData.data[4],
-        congType: responseData.data[3],
-        start: responseData.data[1],
-        end: responseData.data[2],
-      }));
-
-      console.log("Loaded Events:", loadedEvents); // Debugging line to verify the loaded events
-      setEvents(loadedEvents);
+      const newEvent = {
+        camera: responseData.data.video_id,
+        congType: responseData.data.is_congestion ? 'Yes' : 'No',
+        start: responseData.data.congestion_start_time,
+        end: responseData.data.congestion_stop_time,
+      };
+  
+      console.log("New Event:", newEvent); // Debugging line to verify the new event
+  
+      // Check if congType is 'no' and remove the event with the corresponding camera ID
+      if (newEvent.congType === 'No') {
+        setEvents(prevEvents => prevEvents.filter(event => event.camera !== newEvent.camera));
+      } else {
+        // Check if an event with the same camera ID already exists
+        if (!events.some(event => event.camera === newEvent.camera)) {
+          // If not, add the new event to the existing events
+          setEvents(prevEvents => [...prevEvents, newEvent]);
+        }
+      }
     }
-  }, [responseData]);
-
+  }, [responseData, events]); // Include events in the dependency array
+  
+  
 
   return (
     <section>
